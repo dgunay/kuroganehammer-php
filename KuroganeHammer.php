8f2766e34a78b5186d66936d713c959a4fb2040a
root <root@ip-172-31-28-32.us-west-1.compute.internal>
2017-08-18 23:25:48 +0000

    Remove redundant functions from KuroganeHammer class

--- KuroganeHammer.php
+++ KuroganeHammer.php
@@ -5,7 +5,10 @@ foreach (glob("EndpointFamilies/*.php") as $filename){
 }
 
 /**
- * The KuroganeHammer class.
+ * The KuroganeHammer class. Allows access to the entire KuroganeHammer API
+ * via its properties
+ * 
+ * @example $kh->characters('Marth); Get simple data about Marth.
  * 
  * TODO: Rewrite documentation for all endpoints.
  * TODO: Add BadMethodCallExceptions for endpts requiring id/name.
@@ -13,6 +16,42 @@ foreach (glob("EndpointFamilies/*.php") as $filename){
  */
 class KuroganeHammer
 {
+    
+    /**
+     * Angles class, containing functions for each /angles endpoint
+     *
+     * @var Angles
+     */
+    public $angles;
+    
+    /**
+     * BaseDamages class, containing functions for each /baseDamages endpoint
+     *
+     * @var BaseDamages
+     */
+    public $baseDamages;
+    
+    /**
+     * BaseKnockbacks class, containing functions for each /baseKnockbacks endpoint
+     *
+     * @var BaseKnockbacks
+     */
+    public $baseKnockbacks;
+    
+    /**
+     * CharacterAttributes class, containing functions for each /characterAttributes endpoint
+     *
+     * @var CharacterAttributes
+     */
+    public $characterAttributes;
+    
+    /**
+     * CharacterAttributeTypes class, containing functions for each /characterAttributeTypes endpoint
+     *
+     * @var CharacterAttributeTypes
+     */
+    public $characterAttributeTypes;
+    
     /**
      * Characters class, containing functions to get /Characters endpoints
      *
@@ -21,74 +60,74 @@ class KuroganeHammer
     public $characters;
     
     /**
-     * Undocumented variable
+     * FirstActionableFrames class, containing functions for each /firstActionableFrames endpoint
      *
-     * @var [type]
-     */
-    public $angles;
-    
-    /**
-     * Undocumented variable
-     *
-     * @var [type]
-     */
-    public $baseDamages;
-    
-    /**
-     * Undocumented variable
-     *
-     * @var [type]
-     */
-    public $baseKnockbacks;
-    
-    /**
-     * Undocumented variable
-     *
-     * @var [type]
-     */
-    public $characterAttributes;
-    
-    /**
-     * Undocumented variable
-     *
-     * @var [type]
-     */
-    public $characterAttributeTypes;
-    
-    /**
-     * Undocumented variable
-     *
-     * @var [type]
+     * @var FirstActionableFrames
      */
     public $firstActionableFrames;
     
     /**
-     * Undocumented variable
+     * Hitboxes class, containing functions for each /hitboxes endpoint
      *
-     * @var [type]
+     * @var Hitboxes
      */
     public $hitboxes;
     
     /**
-     * Undocumented variable
+     * KnockbackGrowths class, containing functions for each /knockbackGrowths endpoint
      *
-     * @var [type]
+     * @var KnockbackGrowths
      */
     public $knockbackGrowths;
     
     /**
-     * Undocumented variable
+     * Movements class, containing functions for each /movements endpoint
      *
-     * @var [type]
+     * @var Movements
      */
     public $movements;
     
     /**
-     * Undocumented variable
+     * Moves class, containing functions for each /moves endpoint
      *
-     * @var [type]
+     * @var Moves
      */
     public $moves;
+
+    /**
+     * Notations class, containing functions for each /notations endpoint
+     *
+     * @var Notations
+     */
+    public $notations;
+
+    /**
+     * SetKnockbacks class, containing functions for each /setKnockbacks endpoint
+     *
+     * @var SetKnockbacks
+     */
+    public $setKnockbacks;
+
+    /**
+     * SmashAttributeTypes class, containing functions for each /smashAttributeTypes endpoint
+     *
+     * @var SmashAttributeTypes
+     */
+    public $smashAttributeTypes;
+
+    /**
+     * Throws class, containing functions for each /throws endpoint
+     *
+     * @var Throws
+     */
+    public $throws;
+
+    /**
+     * ThrowTypes class, containing functions for each /throwTypes endpoint
+     *
+     * @var ThrowTypes
+     */
+    public $throwTypes;
     
 
     function __construct()
@@ -111,212 +150,11 @@ class KuroganeHammer
 
     }
 
-    function getDetailedMoves($id = null){
-        if ($id === null){
-            return $this->get('/api/DetailedMoves');
-        }
-        return $this->charactersRequest($id, 'detailedmoves');
-    }
-
-    
-    
-    function getThrows($id = null){
-        if ($id === null){
-            return $this->get('/api/Throws');
-        }
-        return $this->charactersRequest($id, 'throws');        
-    }
-    
-    function getCharacterAttributes($id = null){
-        $path = '/api/CharacterAttributes';
-        if ($id !== null){
-            if (is_numeric($id)){
-                $path .= '/' . $id;
-            }
-            else {
-                $path .= '/name/' . $id;
-            }
-        }
-        return $this->get($path);
-    }
-    
-    function getCharacterAttributeTypes($id = null){
-        $path = '/api/characterattributetypes';
-        if ($id !== null){
-            $path .= '/' . $id;
-        }
-        return $this->get($path);
-    }
-    
-    function getFirstActionableFrames($id = null){
-        $path = '/api/FirstActionableFrames';
-        if ($id !== null){
-            $path .= '/' . $id;
-        }
-        return $this->get($path);
-    }
-    
-    function getHitboxes($id = null){
-        $path = '/api/Hitboxes';
-        if ($id !== null){
-            $path .= '/' . $id;
-        }
-        return $this->get($path);
-    }
-    
-    function getKnockbackGrowths($id = null){
-        $path = '/api/KnockbackGrowths';
-        if ($id !== null){
-            $path .= '/' . $id;
-        }
-        return $this->get($path);    
-    }
-    
-    function getMovements($id = null){
-        $path = '/api/movements';
-        if ($id !== null){
-            if (is_numeric($id)){
-                $path .= '/' . $id;
-            }
-            else {
-                $path .= '/name/' . $id;
-            }
-        }
-        return $this->get($path);
-    }
-    
-    function getMoves($id = null){
-        if ($id === null){
-            return $this->get('/api/moves');
-        }
-        return $this->movesRequest($id);        
-    }
-
-    private function movesRequest($id = null, $endpoint = null){
-        $path = '/api/moves';
-        if ($id !== null) {
-            if (!is_numeric($id)) {
-                $path .= '/name';
-            }
-            
-            $path .= '/' . $id;
-        }
-        
-        if ($endpoint !== null) {
-            $path .= '/'.$endpoint;
-        }
-        
-        return $this->get($path);
-    }
-
-    function getSmashAttributeType($type){
-        // TODO: Figure out what the heck this API endpoint does
-    }
-    
-    function getAngles($id = null){
-        $path = '/api/Angles';
-        if ($id !== null){
-            $path .= '/'.$id;
-        }
-        return $this->get($path);        
-    }
-    
-    function getNotations($id = null){
-        if ($id === null){
-            return $this->get('/api/notations');
-        }
-        return $this->charactersRequest($id, 'notations');        
-    }
-    
-    
-    
-    
-    function getBaseKnockbacks($id = null){
-        $path = '/api/BaseKnockbacks';
-        if ($id !== null){
-            $path .= '/' . $id;
-        }
-        return $this->get($path);        
-    }
-    
-    function getBaseDamages($id = null){
-        $path = '/api/BaseDamages';
-        if ($id !== null){
-            $path .= '/' . $id;
-        }
-        return $this->get($path);
-    }
-    
-    
-    
-    /**
-     * Function to execute generalized GET requests.
-     * @param string $path API path in form '/api/endpoint'.
-     * @throws Exception if failed to decode JSON.
-     * @return array The API's response decoded as a PHP array, whether the
-     *  resource is found or not (an error message and HTTP code are returned in
-     *  an array if not.)
-     */
-    private function get($path)
-    {
-        $url = self::API_URL_BASE . $path;
-
-        curl_setopt($this->curl_handle, CURLOPT_URL, $url);
-        $response = curl_exec($this->curl_handle);
-        $http_code = curl_getinfo($this->curl_handle, CURLINFO_HTTP_CODE);
-        
-        // 404 errors do not return JSON, so we handle them first
-        if ($http_code == 404){
-            return array(
-                'message'   => 'File or directory not found.',
-                'httpCode'  => 404,
-            );
-        }
-
-        try {
-            $response = $this->json_decode_with_exception($response);
-        } catch(Exception $e) {
-            throw $e;
-        }
-
-        if ($http_code != 200) {
-            $response['httpCode'] = $http_code;
-            return $response;
-        }
-
-        return $response;
-    }
-
-    /**
-     * Queries the API at an endpoint explicitly defined by the function arg.
-     *
-     * @param string $path The API path.
-     * @return void
-     */
     function customGet($path){
-        return $this->get($path);
-    }
-
-    /**
-     * Calls json_decode() with assoc = true, but throws an exception if it
-     * doesn't work.
-     *
-     * @param string $json_str
-     * @throws Exception if failure to decode.
-     * @return array Results decoded into a PHP array.
-     */
-    private function json_decode_with_exception($json_str)
-    {
-        $result = json_decode($json_str, true);
-
-        if (json_last_error() != 0) {
-            throw new RuntimeException('Failed to decode JSON.');
-        }
-
-        return $result;
+        return (EndpointFamily::get($path));
     }
 }
 
 $kh = new KuroganeHammer();
 
-print_r($kh->moves->autocancel());
+print_r($kh->customGet('/api/angles'));
